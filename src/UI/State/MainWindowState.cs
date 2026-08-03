98dd5608edbd2273a3325a0a230ae754bce009d7
Bobjobob24 <bobsgaming24@gmail.com>
2026-08-03 01:59:13 -0500

    Removed redundant flyout states

--- src/UI/State/MainWindowState.cs
+++ src/UI/State/MainWindowState.cs
@@ -4,9 +4,6 @@ namespace PakMaster.UI.State
     {
         private bool _isAppSettingsFlyoutOpen;
         private bool _isAboutFlyoutOpen;
-        private bool _isAesKeysFlyoutOpen;
-        private bool _isIoStoreFlyoutOpen;
-        private bool _isRepakSettingsFlyoutOpen;
 
         public ObservableCollection<SidebarModel> MenuItems { get; set; }
         public ObservableCollection<SidebarModel> OptionsMenuItems { get; set; }
@@ -18,7 +15,7 @@ namespace PakMaster.UI.State
             // Top Menu Items
             MenuItems =
             [
-                new SidebarModel
+                /*new SidebarModel
                 {
                     TextGetter = () => Lang.AESKeys_Title,
                     Icon = "ShieldKey",
@@ -29,7 +26,7 @@ namespace PakMaster.UI.State
                     TextGetter = () => Lang.RepakSettings_Title,
                     Icon = "AlphaRBox",
                     Command = new RelayCommand((sender, e) => SidebarRepakSettings_Click(sender, e))
-                }/*,
+                },
                 new SidebarModel
                 {
                     TextGetter = () => Lang.ZenToolsSettings_Title,
@@ -54,79 +51,6 @@ namespace PakMaster.UI.State
                     Command = new RelayCommand((sender, e) => SidebarAbout_Click(sender, e))
                 }
             ];
-        }
-
-        // ============ Legacy Flyouts ============
-        private void SidebarAESKeys_Click(object? sender, RoutedEventArgs e)
-        {
-            OpenAesKeysFlyout();
-        }
-
-        private void SidebarRepakSettings_Click(object? sender, RoutedEventArgs e)
-        {
-            OpenRepakSettingsFlyout();
-        }
-
-        private void SidebarZenToolsSettings_Click(object? sender, RoutedEventArgs e)
-        {
-            System.Windows.MessageBox.Show("This feature is not finished.", "Work In Progress", System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Information);
-        }
-
-        public bool IsAesKeysFlyoutOpen
-        {
-            get => _isAesKeysFlyoutOpen;
-            set
-            {
-                if (_isAesKeysFlyoutOpen != value)
-                {
-                    _isAesKeysFlyoutOpen = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-
-        public void OpenAesKeysFlyout()
-        {
-            if (IsAesKeysFlyoutOpen) IsAesKeysFlyoutOpen = false;
-            IsAesKeysFlyoutOpen = true;
-        }
-
-        public bool IsIoStoreFlyoutOpen
-        {
-            get => _isIoStoreFlyoutOpen;
-            set
-            {
-                if (_isIoStoreFlyoutOpen != value)
-                {
-                    _isIoStoreFlyoutOpen = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-
-        public void OpenIoStoreFlyout()
-        {
-            if (IsIoStoreFlyoutOpen) IsIoStoreFlyoutOpen = false;
-            IsIoStoreFlyoutOpen = true;
-        }
-
-        public bool IsRepakSettingsFlyoutOpen
-        {
-            get => _isRepakSettingsFlyoutOpen;
-            set
-            {
-                if (_isRepakSettingsFlyoutOpen != value)
-                {
-                    _isRepakSettingsFlyoutOpen = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-
-        public void OpenRepakSettingsFlyout()
-        {
-            if (IsRepakSettingsFlyoutOpen) IsRepakSettingsFlyoutOpen = false;
-            IsRepakSettingsFlyoutOpen = true;
         }
 
         // ============ App Settings Flyout ============
